d70a8204fd7ae6db147de1a27801fc0846693779
Monika <monika.trzaskowska.bur@gmail.com>
2019-09-11 23:49:05 +0200

    zadanie 3

---
+++ addres.py
@@ -0,0 +1,24 @@
+class Addres:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, mail, mail2, mail3, homepage, bday, bmonth, byear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.mail = mail
+        self.mail2 = mail2
+        self.mail3 = mail3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
