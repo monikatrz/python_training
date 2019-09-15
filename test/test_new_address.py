2b9408f9bdac4c1881362ded4f005cb26f731341
Monika <monika.trzaskowska.bur@gmail.com>
2019-09-15 11:17:56 +0200

    wyodrebniona klasa do kierowania sesjami

--- test/test_new_address.py
+++ test/test_new_address.py
@@ -10,7 +10,7 @@ def app(request):
     return fixture
 
 def test_add_new_address(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_address(Addres(firstname="aaaa", middlename="mmmm", lastname="bbbbb", nickname="abacdccc", title="tttttttt", company="cccccccccccc", address="adrrr", home="123456789", mobile="111222333", work="wwwwwww", fax="ffffff", mail='mail@wp.pl', mail2="mail2@wp.pl", mail3="mail3@wp.pl", homepage="hhhh", address2="23233", phone2="1234567890", notes="notttttttt"))
-    app.logout()
+    app.session.logout()
 
