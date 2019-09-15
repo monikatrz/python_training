f70974553e5b30d9c4491c9ccccc0bd9dd19816a
Monika <monika.trzaskowska.bur@gmail.com>
2019-09-15 11:53:37 +0200

    zadanie 5 (wyodrebiona klasa do pracy z grupami i kontaktami)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,10 +11,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="kkkkkk", header="kkkkkkkkkkkkk", footer="kkkkkkkkkkkkkkkk"))
+    app.group.create(Group(name="kkkkkk", header="kkkkkkkkkkkkk", footer="kkkkkkkkkkkkkkkk"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
