50aee1d89dc6a7f6455ede9eb2d3414003cb01be
Monika <monika.trzaskowska.bur@gmail.com>
2019-10-13 23:25:43 +0200

    zadanie 16 - parametryzaja (przegladarka, url, username, password)

--- conftest.py
+++ conftest.py
@@ -8,12 +8,16 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 @pytest.fixture(scope="session", autouse = True)
@@ -23,3 +27,9 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--username", action="store", default="admin")
+    parser.addoption("--password", action="store", default="secret")
